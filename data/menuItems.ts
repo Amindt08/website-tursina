c5a19c029db87554cd54c4ff27606242c3bcd65a
AMINDTMSTR\User <amindtmstr>
2025-09-23 21:59:54 +0700

    add asset

---
+++ data/menuItems.ts
@@ -0,0 +1,116 @@
+export const menuItems = [
+    {
+        title: "Kebab Original Mini",
+        price: "Rp17.000",
+        image: "/images/menu/9.jpg",
+        category: "Kebab"
+    },
+    {
+        title: "Kebab Original Normal",
+        price: "Rp17.000",
+        image: "/images/menu/10.jpg",
+        category: "Kebab"
+    },
+    {
+        title: "Kebab Original Big",
+        price: "Rp17.000",
+        image: "/images/menu/1.jpg",
+        category: "Kebab"
+    },
+    {
+        title: "Kebab Telur",
+        price: "Rp25.000",
+        image: "/images/menu/7.jpg",
+        category: "Kebab"
+    },
+    {
+        title: "Kebab Sosis",
+        price: "Rp30.000",
+        image: "/images/menu/8.jpg",
+        category: "Kebab"
+    },
+    {
+        title: "Spicy Kebab",
+        price: "Rp35.000",
+        image: "/images/menu/4.jpg",
+        category: "Kebab"
+    },
+    {
+        title: "Black Kebab",
+        price: "Rp20.000",
+        image: "/images/menu/6.jpg",
+        category: "Kebab"
+    },
+    {
+        title: "Cheese Kebab",
+        price: "Rp27.000",
+        image: "/images/menu/2.jpg",
+        category: "Kebab"
+    },
+    {
+        title: "Beef Full Kebab",
+        price: "Rp15.000",
+        image: "/images/menu/5.jpg",
+        category: "Kebab"
+    },
+    {
+        title: "Black Beef Full Kebab",
+        price: "Rp30.000",
+        image: "/images/menu/3.jpg",
+        category: "Kebab"
+    },
+    {
+        title: "Beef Burger",
+        price: "Rp30.000",
+        image: "/images/menu/11.jpg",
+        category: "Burger"
+    },
+    {
+        title: "Black Burger",
+        price: "Rp30.000",
+        image: "/images/menu/12.jpg",
+        category: "Burger"
+    },
+    {
+        title: "Cheese Burger",
+        price: "Rp30.000",
+        image: "/images/menu/14.jpg",
+        category: "Burger"
+    },
+    {
+        title: "Black Cheese Burger",
+        price: "Rp30.000",
+        image: "/images/menu/13.jpg",
+        category: "Burger"
+    },
+    {
+        title: "Maryam Original",
+        price: "Rp30.000",
+        image: "/images/menu/15.jpg",
+        category: "Maryam"
+    },
+    {
+        title: "Maryam Coklat",
+        price: "Rp30.000",
+        image: "/images/menu/16.jpg",
+        category: "Maryam"
+    },
+    {
+        title: "Maryam Coklat Keju",
+        price: "Rp30.000",
+        image: "/images/menu/17.jpg",
+        category: "Maryam"
+    },
+    {
+        title: "Teh Pucuk",
+        price: "Rp30.000",
+        image: "/images/menu/18.jpg",
+        category: "Minuman"
+    },
+    {
+        title: "Teh Tarik",
+        price: "Rp30.000",
+        image: "/images/menu/19.jpg",
+        category: "Minuman"
+    },
+];
